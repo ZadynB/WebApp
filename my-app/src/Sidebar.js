fd23a5049d708753bbcc2c40caaa8b96bd7e61ea
Zadyn Belgrave <zadynbelgrave@gmail.com>
2024-01-25 22:16:35 -0500

    added sidebar component

--- my-app/src/Sidebar.js
+++ my-app/src/Sidebar.js
@@ -1,20 +1,67 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from '@mui/material';
-import { ArrowForwardIos } from '@mui/icons-material';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import KeyboardArrowUp from '@mui/icons-material/KeyboardArrowUp';
+import Info from '@mui/icons-material/Info';
+import GitHub from '@mui/icons-material/GitHub';
 
 function Sidebar() {
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Developers', 'Github'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <Info /> : <GitHub />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
-    <div className='App-body-item App-body-sidebar'>
-      <div className='Sidebar'>
-        <div className='Sidebar-txt'>
-          <p>Some Text</p>
-        </div>
-        <Button color='primary' variant='contained' className='Sidebar-btn'>
-          <ArrowForwardIos/>
-        </Button>
-      </div>
+    <div className='sidebar'>
+      {['left'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button size='small' variant='outlined' onClick={toggleDrawer(anchor, true)} className='rotate'>
+              <KeyboardArrowUp/>
+          </Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
     </div>
   );
 }
