e27bdd8e6092430ead243844a814b761eada18d5
Zadyn Belgrave <zadynbelgrave@gmail.com>
2024-02-04 20:44:05 -0500

    updated the carousel styling and added sections to the projectsContainer

--- my-app/src/components/Carousel.js
+++ my-app/src/components/Carousel.js
@@ -8,8 +8,9 @@ import Info from '@mui/icons-material/Info';
 function Carousel(props) {
   const data = props.data.slides;
   const [activeState, setActiveState] = useState({index: 0, direction: ''});
-  const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState({item: false, info: false});
   const presentableData = data[activeState.index];
+  const lightBlue = getComputedStyle(document.body).getPropertyValue('--light-blue2');
 
   const handleClick = (dir) => {
     if (dir === 'up') {
@@ -32,11 +33,22 @@ function Carousel(props) {
   });
 
   // change scale of slide
-  const springRef = useSpringRef();
-  const spring = useSpring({
-    from: isHovered ? { opacity: 1, scale: 1} : { opacity: 1, scale: 1.2},
-    to: isHovered ? { opacity: 1, scale: 1.2} : { opacity: 1, scale: 1},
-    ref: springRef,
+  const itemSpringRef = useSpringRef();
+  const itemSpring = useSpring({
+    from: isHovered.item ? { opacity: 1, scale: 1} : { opacity: 1, scale: 1.2},
+    to: isHovered.item ? { opacity: 1, scale: 1.2} : { opacity: 1, scale: 1},
+    ref: itemSpringRef,
+    config: {
+      friction: 10,
+      tension: 120
+    }
+  });
+
+  const infoSpringRef = useSpringRef();
+  const infoSpring = useSpring({
+    from: isHovered.info ? { opacity: 1, scale: 1, backgroundColor: 'white'} : { opacity: 1, scale: 1.2, backgroundColor: lightBlue},
+    to: isHovered.info ? { opacity: 1, scale: 1.2, backgroundColor: lightBlue} : { opacity: 1, scale: 1, backgroundColor: 'white'},
+    ref: infoSpringRef,
     config: {
       friction: 10,
       tension: 120
@@ -48,8 +60,12 @@ function Carousel(props) {
   }, [activeState, transRef]);
 
   useEffect(() => {
-    springRef.start();
-  }, [isHovered, springRef]);
+    itemSpringRef.start();
+  }, [isHovered.item, itemSpringRef]);
+
+  useEffect(() => {
+    infoSpringRef.start();
+  }, [isHovered.info, infoSpringRef]);
 
   const createComponent = (index) => {
     let properties = {'name': index.content.name};
@@ -59,22 +75,33 @@ function Carousel(props) {
 
   return (
     <div className='slide-container'>
-      <Button size='small' onClick={() => handleClick('up')}>
+      <Button style={{margin: 'auto'}} size='small' onClick={() => handleClick('up')}>
         <KeyboardArrowUp />
       </Button>
-      <animated.div style={{...spring, height: '100%', padding: '5px 5px', zIndex: 5}} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+      <animated.div 
+        style={{...itemSpring, height: '100%', padding: '5px 5px', zIndex: 5, cursor: 'pointer'}}
+        onClick={() => {}}
+        onMouseEnter={() => setIsHovered({item: true, info: false})}
+        onMouseLeave={() => setIsHovered({item: false, info: false})}
+      >
         {trans((style, index) => (
           <animated.div style={{...style, position: 'relative', height: '100%'}}>
             {createComponent(index)}
           </animated.div>
         ))}
       </animated.div>
-      <Button size='small' onClick={() => handleClick('down')}>
+      <Button style={{margin: 'auto'}} size='small' onClick={() => handleClick('down')}>
         <KeyboardArrowDown />
       </Button>
-      <Button size='small' className='project-info-btn'>
-        <Info />
-      </Button>
+      <animated.div
+        style={{...infoSpring, margin: 'auto', borderRadius: '5px'}}
+        onMouseEnter={() => setIsHovered({item: false, info: true})}
+        onMouseLeave={() => setIsHovered({item: false, info: false})}
+      >
+        <Button size='small' className='project-info-btn'>
+          <Info />
+        </Button>
+      </animated.div>
     </div>
   );
 }
--- my-app/src/components/ProjectContainer.js
+++ my-app/src/components/ProjectContainer.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 // import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Grid } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import { motion } from 'framer-motion';
 import StackedNavbar from './StackedNavbar';
@@ -15,16 +16,44 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Info from '@mui/icons-material/Info';
+import Paper from '@mui/material/Paper';
 
 function ProjectContainer(props) {
+  const blue2 = getComputedStyle(document.body).getPropertyValue('--blue2');
 
   return (
     <div className='project-container'>
       <React.Fragment key={props.name}>
-          <div className='project_info'>
-            <p style={{margin: 0}}>{props.name}</p>
+        <div className='project-info'>
+          <Grid container>
+            <Grid item sm={12} md={8}>
+              <Paper>{props.name}</Paper>
+            </Grid>
+            <Grid item sm={0} md={4}>
+              <Paper>Date</Paper>
+            </Grid>
+          </Grid>
+          <Grid container>
+            <Grid item md={12}>
+              <Paper>Description</Paper>
+            </Grid>
+          </Grid>
+          <Grid container>
+            {/* add code to render a grid item for each tool used */}
+            {/* to adjust the height of the paper add a height css parameter */}
+            <Grid item >
+              <Paper>CSS</Paper>
+            </Grid>
+          </Grid>
+          {/* <div className='project-header'>
+            <p>{props.name}</p>
+            <p>Date</p>
           </div>
-        </React.Fragment>
+          <div className='project-desc'>
+            <p>some text</p>
+          </div> */}
+        </div>
+      </React.Fragment>
     </div>
   );
 }
